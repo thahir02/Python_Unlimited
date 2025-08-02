9d7fd1dba25f1ad7572d84098cbcd0bd63e506a0
SM Thahir <sm20818918@gmail.com>
2025-08-02 17:03:29 +0530

    Initial commit

---
+++ chapter.py
@@ -0,0 +1,5 @@
+# Module : A module is a flie containing code written by somebody else(usually) which can be imported and used in our progarms.for example modules like flask,django,etc
+# PIP : Pip is the package manager for python.You can use pip to install a module on your system.
+import pyjokes
+joke = pyjokes.get_joke()
+print(joke)
