6241ab438d5b3492555e9e8fd9dcd47c446d3798
SM Thahir <155128623+thahir02@users.noreply.github.com>
2026-07-02 18:40:20 +0530

    Refactor sum calculation using continue statement
    Refactor the sum calculation to use 'continue' instead of 'else' for odd/even separation.

--- sum_odd_or_even.py
+++ sum_odd_or_even.py
@@ -8,3 +8,16 @@ for i in range(1,n+1):
 
 print('odd_sum = ',odd_sum)
 print('even_sum =',even_sum)
+
+# The above same program can be written using contiue instead of else block i.e ;
+
+n=int(input())
+odd_sum,even_sum=0,0
+for i in range(1,n+1):
+    if i%2==0:
+        even_sum +=i
+        continue
+    odd_sum +=i
+
+print('odd_sum = ',odd_sum)
+print('even_sum =',even_sum)
