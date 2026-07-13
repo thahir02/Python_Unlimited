a435385350e0f4f5cb26f37d82751f77ce6db7bb
SM Thahir <155128623+thahir02@users.noreply.github.com>
2026-07-13 15:34:17 +0530

    Add palindrome check functionality

---
+++ palindrome.py
@@ -0,0 +1,6 @@
+str1=input('enter a string : ').lower() #lower() is optional which can even use to check the case sesnsitive strings
+if str1==str1[::-1]:
+    print('Palindrome')
+else:
+    print('Not Palindrome')
+    
